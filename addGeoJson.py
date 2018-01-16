cb49360d1b34fbf1c616adf9cb3bb2fb49dd9de4
TichyProgs <33847650+TichyProgs@users.noreply.github.com>
2018-01-17 00:19:09 +0100

    got rid of unnecessary code

--- addGeoJson.py
+++ addGeoJson.py
@@ -1,24 +1,24 @@
 import folium
-import numpy as np
 
+mapa = folium.Map(width=800, height=600, zoom_start=5.5,
+                  location=[52, 19], tiles='Mapbox Bright')
 
+powiaty_geojson = "geo_put.json"
 
-mapa = folium.Map(width=650, height=500, zoom_start=5,
-                  location=[52, 19]).                  # Bolzga's location
-
-powiaty_geojson = "geo_put.json".  # the geojson file
 
 folium.GeoJson(
     powiaty_geojson,
     name='geojson',
-    style_function=lambda feature: {    # I don't know why this doesn't work and show the counties' borders on map
+    style_function=lambda feature: {
         'color': 'black',
-        'weight': 4,
+        'fillColor': '#fff',
+        'weight': 1,
         'stroke': True,
         # 'dashArray': '5, 5'
     },
-
 ).add_to(mapa)
 folium.LayerControl().add_to(mapa)
 
-# mapa.save("index.html")
+
+mapa.save("index.html")
+
