7cea81b5dccf2d37ee7f2dc1f79210620614eb52
TichyProgs <33847650+TichyProgs@users.noreply.github.com>
2018-01-17 09:44:32 +0100

    now it works correctly
    while I was reading the geojson file I noticed that polish signs aren't coded in a way I thought they will be. Meaning something like this "\u0141owicz" instead of "Łowicz". still can't correct this but maybe it doesn't matter actually

--- shape2geoJson.py
+++ shape2geoJson.py
@@ -5,18 +5,16 @@ from json import dumps
 # code from https://ocefpaf.github.io/python4oceanographers/blog/2015/02/02/cartopy_folium_shapefile/
 # but has some changes
 
-def shape2json(fname, outfile="geo_put.json"):
+def shape2json(fname, outfile="gedecoded.json"):
     reader = shapefile.Reader(fname)
     fields = reader.fields[1:]
     field_names = [field[0] for field in fields]
-
     data = []
     for sr in reader.shapeRecords():
+        geom = sr.shape.__geo_interface__
         atr = dict(zip(field_names, sr.record))
-        data.append(dict(type="Feature", geometry=geom, properties=atr))
+        data.append(dict(type="Feature", geometry=geom, properties=atr
             
-    keys = ['CELLCODE', 'EOFORIGIN', 'NOFORIGIN']
-
     with open(outfile, "w") as geojson:
         geojson.write(dumps({"type": "FeatureCollection",
                              "features": data}, indent=2) + "\n")
